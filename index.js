a42e707b612754791fae90a573d4c6f6c98285ab
Alex Edwards <anedwards180@gmail.com>
2021-08-12 17:45:47 -0500

    commit tree 99af73f01881ced703867664dba7d26af53cc747

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(recordArr){
+
+   const winningSeason = recordArr.find(season => season.result === 'W')
+
+   console.log(winningSeason)
+
+   return !!winningSeason ? winningSeason.year : undefined
+
+
+  
+}
+
 
